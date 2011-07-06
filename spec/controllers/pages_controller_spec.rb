0e46ab5c7aaedc36d8256300910523634b740c45
vidhoonv <vidhu2366@gmail.com>
2011-07-06 18:32:47 +0530

    Added stylesheets

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+
+ before(:each) do
+   @base_title = "Ruby on Rails Tutorial Sample App"
+ end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -9,6 +14,11 @@ describe PagesController do
     end
   end
 	
+  it "should have the right title" do
+    get 'home'
+    response.should have_selector("title",:content => "#{@base_title} | Home")
+  end 
+
   it "should have a non-blank body" do
      get 'home'
      response.body.should_not =~ /<body>\s*<\/body>/
@@ -22,6 +32,16 @@ describe PagesController do
     end
   end
 
+ it "should have the right title" do
+    get 'contact'
+    response.should have_selector("title",:content => "#{@base_title} | Contact")
+  end 
+
+  it "should have a non-blank body" do
+     get 'contact'
+     response.body.should_not =~ /<body>\s*<\/body>/
+    end
+
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
@@ -29,4 +49,13 @@ describe PagesController do
     end
   end
 
+ it "should have the right title" do
+    get 'about'
+    response.should have_selector("title",:content => "#{@base_title} | About")
+  end 
+
+  it "should have a non-blank body" do
+     get 'about'
+     response.body.should_not =~ /<body>\s*<\/body>/
+    end
 end
